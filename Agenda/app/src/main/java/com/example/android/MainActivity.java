91974ff5792b5dc1954a1d0f9ad3fca467d2b5fd
Cleonildo Soares Guimaraes Junior <cleonildo.junior@b2wdigital.com>
2021-06-25 00:47:32 -0300

    ListView

--- Agenda/app/src/main/java/com/example/android/MainActivity.java
+++ Agenda/app/src/main/java/com/example/android/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +17,9 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Junior", "Veronica", "Alex", "Luiz", "Carlos"));
+        ListView listaAlunos =  findViewById(R.id.activity_main_lista_alunos);
+        listaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+
     }
 }
