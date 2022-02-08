01ee6e0267e80acbedf431f597980ebc31908bf2
CleuJunior <cleu.junior@gmail.com>
2022-02-07 23:34:58 -0300

    Activity Formulario - Adicionar Alunos

--- Agenda/app/src/main/java/com/example/android/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/com/example/android/ui/activity/ListaAlunosActivity.java
@@ -1,6 +1,5 @@
-package com.example.android;
+package com.example.android.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -8,19 +7,21 @@ import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.android.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Junior", "Veronica", "Alex", "Luiz", "Carlos"));
-        ListView listaAlunos =  findViewById(R.id.activity_main_lista_alunos);
+        ListView listaAlunos =  findViewById(R.id.activity_main_lista_alunos_listview);
         listaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
 
     }
