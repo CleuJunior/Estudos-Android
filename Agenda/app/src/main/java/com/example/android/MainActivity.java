6fb9af50a8d199b2cccde9aa510b7652adeec207
CleuJunior <cleu.junior@gmail.com>
2022-02-07 22:30:52 -0300

    Adicionando botoes de Add

--- Agenda/app/src/main/java/com/example/android/MainActivity.java
+++ Agenda/app/src/main/java/com/example/android/MainActivity.java
@@ -6,17 +6,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Junior", "Veronica", "Alex", "Luiz", "Carlos"));
         ListView listaAlunos =  findViewById(R.id.activity_main_lista_alunos);
         listaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
